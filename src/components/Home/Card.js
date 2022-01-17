eb43558a19ce8b6ccde633b0365a8e4b133170ed
Daniel Apantaku <apantakuayo@gmail.com>
2022-01-17 11:00:00 +0100

    almost done

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import { useLocation } from "react-router-dom";
 // import { useDispatch } from "react-redux";
 // import { isLiked } from "../../redux/actions/likeActions";
 
 const Card = ({ postsReducer }) => {
   // const dispatch = useDispatch();
+  // const location = useLocation();
+  // const id = location.pathname.split("/")[2];
   const [isLiked, setIsLiked] = useState(false);
   const handleToggle = () => {
     setIsLiked(!isLiked);
     localStorage.setItem("isLiked", JSON.stringify(isLiked));
     console.log(isLiked);
   };
-
+  // console.log(location.pathname)
   const storageData = JSON.parse(localStorage.getItem("isLiked"));
   useEffect(() => {
     if (localStorage.getItem(isLiked)) {
@@ -20,9 +23,13 @@ const Card = ({ postsReducer }) => {
     }
     return;
   }, [storageData, isLiked]);
-
+  // style={{ width: "23%" }}
+  const grid = {
+    display: "grid",
+    gridTemplateColumns: "3fr 1fr",
+  };
   return (
-    <div style={{ width: "23%" }} className="mx-2 cursor-pointer">
+    <div className="mx-2 w-full sm:w-1/4 cursor-pointer">
       <div className="w-full border border-0 border-solid border-black">
         <div>
           <div className="h-40">
@@ -33,8 +40,8 @@ const Card = ({ postsReducer }) => {
             ></img>
           </div>
 
-          <div className="mt-1 grid grid-cols-2 ">
-            <div className="grid-cols-1 grid ">
+          <div style={grid} className="mt-1 grid">
+            <div className="flex justify-center items-center">
               <span className="text-xs" style={{ color: "gray" }}>
                 Credit: {postsReducer.posts.copyright}
               </span>
@@ -56,8 +63,10 @@ const Card = ({ postsReducer }) => {
         </div>
         <div>
           <div>
-            <h2 className="mt-3 font-bold">{postsReducer.posts.title}</h2>
-            <div className="mt-2 text-sm">
+            <h2 style={{ fontSize: "20px" }} className="mt-3 font-bold">
+              {postsReducer.posts.title}
+            </h2>
+            <div className="">
               <span style={{ fontSize: "12px", color: "gray" }}>
                 {postsReducer.posts.date}
               </span>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,29 +1,33 @@
 import React from "react";
 import Heading from "../../components/Home/Heading";
 import Content from "../../components/Home/Content";
-import FullContent from "../../components/Home/FullContent";
+// import FullContent from "../../components/Home/FullContent";
 
 const Home = () => {
-  const verticalLine = {
-    border: "1px solid transparent",
-    backgroundColor: "rgba(80, 111, 130, 0.1)",
-    height: "100%",
-    position: "static",
-  };
+  // const verticalLine = {
+  //   border: "1px solid transparent",
+  //   backgroundColor: "rgba(80, 111, 130, 0.1)",
+  //   height: "100%",
+  //   position: "static",
+  // };
   return (
     <>
       <Heading />
       <div className="flex">
-        <div className="w-9/12">
+        <div className="w-full">
+          <Content />
+        </div>
+
+        {/* <div className="w-9/12">
           <Content />
         </div>
         <div style={{ width: "5px" }}>
           <div className="mx-2 " style={verticalLine}></div>
-        </div>
+        </div> */}
 
-        <div className="w-3/12">
+        {/* <div className="w-3/12">
           <FullContent />
-        </div>
+        </div> */}
       </div>
     </>
   );
